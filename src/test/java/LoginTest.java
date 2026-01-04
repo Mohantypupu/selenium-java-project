f48da6f25f15fb706aa8be4790d6950e7a679e51
Mohantypupu <bikashmohantymca@gmail.com>
2026-01-04 20:19:41 +0530

    Rename T1Test class to LoginTest

--- src/test/java/LoginTest.java
+++ src/test/java/LoginTest.java
@@ -7,7 +7,7 @@ import org.openqa.selenium.chrome.ChromeDriver;
 import io.github.bonigarcia.wdm.WebDriverManager;
 import junit.framework.Assert;
 
-public class T1Test {
+public class LoginTest {
 
 	@Test
 	public void testLogin() {
